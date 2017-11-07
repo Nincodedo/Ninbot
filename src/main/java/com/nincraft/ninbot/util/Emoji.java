229f476750a455367cbbf2702ee5fc2e55a7d01f
Nincodedo <nincodedo@gmail.com>
2017-11-06 18:25:34 -0600

    Add "me too" reaction

--- src/main/java/com/nincraft/ninbot/util/Emoji.java
+++ src/main/java/com/nincraft/ninbot/util/Emoji.java
@@ -8,6 +8,10 @@ public class Emoji {
     public static final String QUESTION_MARK = "\u2274";
     public static final String A = "\uD83C\uDDE6";
     public static final String E = "\uD83C\uDDEA";
+    public static final String H = "\uD83C\uDDED";
+    public static final String K = "\uD83C\uDDF0";
     public static final String M = "\uD83C\uDDF2";
+    public static final String N = "\uD83C\uDDF3";
     public static final String S = "\uD83C\uDDF8";
+    public static final String T = "\uD83C\uDDF9";
 }
--- src/main/java/com/nincraft/ninbot/util/EmojiUtils.java
+++ src/main/java/com/nincraft/ninbot/util/EmojiUtils.java
@@ -1,8 +1,11 @@
 package com.nincraft.ninbot.util;
 
+import lombok.experimental.UtilityClass;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@UtilityClass
 public class EmojiUtils {
     private static Map<String, String> letterMap;
 
@@ -10,8 +13,12 @@ public class EmojiUtils {
         letterMap = new HashMap<>();
         letterMap.put("A", Emoji.A);
         letterMap.put("E", Emoji.E);
+        letterMap.put("H", Emoji.H);
+        letterMap.put("K", Emoji.K);
         letterMap.put("M", Emoji.M);
+        letterMap.put("N", Emoji.N);
         letterMap.put("S", Emoji.S);
+        letterMap.put("T", Emoji.T);
     }
 
     public static String getLetterEmoji(char c) {
