9bbe3eefc27b2730ac23ad2215503c35f4a2263d
Nincodedo <nincodedo@gmail.com>
2017-12-17 01:47:23 -0600

    Make Help command more fancy

--- src/main/java/com/nincraft/ninbot/command/HelpCommand.java
+++ src/main/java/com/nincraft/ninbot/command/HelpCommand.java
@@ -3,7 +3,14 @@ package com.nincraft.ninbot.command;
 import com.nincraft.ninbot.command.util.CommandParser;
 import com.nincraft.ninbot.util.MessageUtils;
 import lombok.val;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class HelpCommand extends AbstractCommand {
 
@@ -16,13 +23,18 @@ public class HelpCommand extends AbstractCommand {
     @Override
     public void executeCommand(MessageReceivedEvent event) {
         val map = CommandParser.getCommandHashMap();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (val key : map.keySet()) {
+        MessageBuilder messageBuilder = new MessageBuilder();
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setTitle("List of commands");
+        embedBuilder.setColor(Color.BLUE);
+        List<String> keyList = new ArrayList<>(map.keySet());
+        Collections.sort(keyList);
+        for (val key : keyList) {
             if (!map.get(key).isHidden()) {
-                stringBuilder.append(map.get(key));
-                stringBuilder.append("\n");
+                embedBuilder.addField(key, map.get(key).getDescription(), false);
             }
         }
-        MessageUtils.sendMessage(event.getChannel(), stringBuilder.toString());
+        messageBuilder.setEmbed(embedBuilder.build());
+        MessageUtils.sendMessage(event.getChannel(), messageBuilder.build());
     }
 }
