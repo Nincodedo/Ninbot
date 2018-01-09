4ee20c67dc4f668155e663ed72bce3785ff7190e
Nincodedo <nincodedo@gmail.com>
2018-01-09 01:31:47 -0600

    Add crit dab

--- src/main/java/com/nincraft/ninbot/command/DabCommand.java
+++ src/main/java/com/nincraft/ninbot/command/DabCommand.java
@@ -1,11 +1,18 @@
 package com.nincraft.ninbot.command;
 
+import com.nincraft.ninbot.entity.EmojiReactionResponse;
 import com.nincraft.ninbot.util.MessageUtils;
 import lombok.val;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
+import java.util.Random;
+
 public class DabCommand extends AbstractCommand {
+
+    private EmojiReactionResponse critResponse = new EmojiReactionResponse("crit");
+    private EmojiReactionResponse dabResponse = new EmojiReactionResponse("dab");
 
     public DabCommand() {
         length = 3;
@@ -25,7 +32,7 @@ public class DabCommand extends AbstractCommand {
             int maxDab = 10;
             for (Message message : channel.getIterableHistory()) {
                 if (message.getAuthor().equals(dabUser)) {
-                    dabOnMessage(message);
+                    dabOnMessage(message, channel);
                     break;
                 }
                 if (count >= maxDab) {
@@ -39,13 +46,20 @@ public class DabCommand extends AbstractCommand {
         }
     }
 
-    private void dabOnMessage(Message message) {
+    private void dabOnMessage(Message message, MessageChannel channel) {
         val guild = message.getGuild();
-
+        Random random = new Random();
+        val critDab = random.nextInt(100) < 5;
+        if (critDab) {
+            critResponse.react(message, channel);
+        }
         for (val emote : guild.getEmotes()) {
             if (emote.getName().contains("dab")) {
                 MessageUtils.addReaction(message, emote);
             }
+        }
+        if (critDab) {
+            dabResponse.react(message, channel);
         }
     }
 }
