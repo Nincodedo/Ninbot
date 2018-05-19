e93591cc1c502c8c1199a7ca1c2bcf3dd257d0d4
Nincodedo <nincodedo@gmail.com>
2018-05-18 23:09:03 -0500

    Add Twitch annoucements
    Closes #20

---
+++ src/main/java/com/nincraft/ninbot/components/twitch/TwitchListener.java
@@ -0,0 +1,40 @@
+package com.nincraft.ninbot.components.twitch;
+
+import com.nincraft.ninbot.components.config.ConfigService;
+import com.nincraft.ninbot.util.MessageUtils;
+import lombok.val;
+import net.dv8tion.jda.core.events.user.update.GenericUserPresenceEvent;
+import net.dv8tion.jda.core.events.user.update.UserUpdateGameEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TwitchListener extends ListenerAdapter {
+
+    private ConfigService configService;
+
+    public TwitchListener(ConfigService configService) {
+        this.configService = configService;
+    }
+
+    @Override
+    public void onGenericUserPresence(GenericUserPresenceEvent event) {
+        if (event instanceof UserUpdateGameEvent) {
+            val updateGameEvent = (UserUpdateGameEvent) event;
+            if (updateGameEvent.getOldGame() != null && updateGameEvent.getNewGame() != null
+                    && updateGameEvent.getOldGame().getUrl() == null && updateGameEvent.getNewGame().getUrl() != null) {
+                val serverId = event.getGuild().getId();
+                val streamingAnnounceUsers = configService.getValuesByName(serverId, "streamingAnnounceUsers");
+                if (streamingAnnounceUsers.contains(event.getMember().getUser().getId())) {
+                    val streamingAnnounceChannel = configService.getSingleValueByName(serverId, "streamingAnnounceChannel");
+                    if (streamingAnnounceChannel.isPresent()) {
+                        val channel = event.getGuild().getTextChannelById(streamingAnnounceChannel.get());
+                        val user = updateGameEvent.getUser().getName();
+                        val url = updateGameEvent.getNewGame().getUrl();
+                        MessageUtils.sendMessage(channel, "%s is streaming! Check them out at %s", user, url);
+                    }
+                }
+            }
+        }
+    }
+}
