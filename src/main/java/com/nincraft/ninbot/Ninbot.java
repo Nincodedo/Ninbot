75ca0aed610d593831f23b6852a1b58c837926db
Nincodedo <nincodedo@gmail.com>
2018-06-03 23:51:18 -0500

    Remove Flyway

--- src/main/java/com/nincraft/ninbot/Ninbot.java
+++ src/main/java/com/nincraft/ninbot/Ninbot.java
@@ -4,7 +4,6 @@ import com.nincraft.ninbot.components.event.EventScheduler;
 import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.Game;
-import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -25,16 +24,9 @@ public class Ninbot {
     @Value("${db.sqliteUrl}")
     private String dbUrl;
 
-    private void migrateDb() {
-        Flyway flyway = new Flyway();
-        flyway.setDataSource(dbUrl, null, null);
-        flyway.migrate();
-    }
-
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext context) {
         return args -> {
-            migrateDb();
             eventScheduler.scheduleAll(jda);
             jda.getPresence().setGame(Game.playing("say \"@Ninbot help\" for list of commands"));
         };
