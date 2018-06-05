db755879f07070ad6dd0ef3acb37b437b913e0ee
Nincodedo <nincodedo@gmail.com>
2018-06-04 20:00:32 -0500

    Refactor

--- src/main/java/com/nincraft/ninbot/components/reaction/EmojiReactionResponse.java
+++ src/main/java/com/nincraft/ninbot/components/reaction/EmojiReactionResponse.java
@@ -1,22 +1,40 @@
 package com.nincraft.ninbot.components.reaction;
 
-import com.nincraft.ninbot.util.EmojiUtils;
 import com.nincraft.ninbot.util.MessageUtils;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EmojiReactionResponse implements IReactionResponse {
+
+    private static Map<String, String> letterMap;
+
+    static {
+        letterMap = new HashMap<>();
+        char unicodeChar = '\uDDE6';
+        char letterChar = 'A';
+        for (int i = 0; i < 26; i++) {
+            letterMap.put(String.valueOf(letterChar), "\uD83C" + unicodeChar);
+            letterChar++;
+            unicodeChar++;
+        }
+    }
 
     private List<String> emojiList;
 
     public EmojiReactionResponse(String response) {
         emojiList = new ArrayList<>();
         for (char c : response.toCharArray()) {
-            emojiList.add(EmojiUtils.getLetterEmoji(c));
+            emojiList.add(getLetterEmoji(c));
         }
+    }
+
+    private static String getLetterEmoji(char c) {
+        return letterMap.get(Character.toString(c).toUpperCase());
     }
 
     @Override
