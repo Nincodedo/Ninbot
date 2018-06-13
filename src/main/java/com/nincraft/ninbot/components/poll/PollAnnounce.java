3a0e98a68d87524606ee2920ae91301deb14c580
Nincodedo <nincodedo@gmail.com>
2018-06-12 22:05:00 -0500

    Refactor poll announcements

---
+++ src/main/java/com/nincraft/ninbot/components/poll/PollAnnounce.java
@@ -0,0 +1,72 @@
+package com.nincraft.ninbot.components.poll;
+
+import com.nincraft.ninbot.util.MessageUtils;
+import lombok.val;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageReaction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimerTask;
+
+public class PollAnnounce extends TimerTask {
+
+    private Poll poll;
+    private Message message;
+
+    PollAnnounce(Poll poll, Message message) {
+        this.poll = poll;
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        announcePollResults();
+    }
+
+    private void announcePollResults() {
+        val newMessage = message.getChannel().getMessageById(message.getId()).complete();
+        int highCount = 0;
+        List<String> winningChoices = new ArrayList<>();
+        List<MessageReaction> reactions = newMessage.getReactions();
+        for (int i = 0; i < reactions.size() && i < poll.getChoices().size(); i++) {
+            MessageReaction reaction = reactions.get(i);
+            if (reaction.getCount() == highCount) {
+                winningChoices.add(poll.getChoices().get(i));
+            }
+            if (reaction.getCount() > highCount) {
+                highCount = reaction.getCount();
+                winningChoices.clear();
+                winningChoices.add(poll.getChoices().get(i));
+            }
+        }
+
+        String pollClosedMessage = " The poll is now closed.";
+        if (highCount <= 1) {
+            poll.setResult("No one voted in this poll." + pollClosedMessage);
+        } else if (winningChoices.size() == 1) {
+            poll.setResult("\"" + winningChoices.get(0) + "\" had the most votes with " + (highCount - 1) + " vote(s)."
+                    + pollClosedMessage);
+        } else if (winningChoices.size() > 1) {
+            poll.setResult(
+                    "It's a tie! " + listWinners(winningChoices) + " won with " + (highCount - 1) + " vote(s) each."
+                            + pollClosedMessage);
+        }
+        message.editMessage(poll.buildClosed()).queue();
+        MessageUtils.sendMessage(message.getChannel(), poll.getResult());
+    }
+
+    private String listWinners(List<String> winningChoices) {
+        StringBuilder winners = new StringBuilder();
+        for (int i = 0; i < winningChoices.size(); i++) {
+            if (i == winningChoices.size() - 1) {
+                winners.append("and ");
+            }
+            winners.append("\"").append(winningChoices.get(i)).append("\"");
+            if (i != winningChoices.size() - 1) {
+                winners.append(", ");
+            }
+        }
+        return winners.toString();
+    }
+}
--- src/main/java/com/nincraft/ninbot/components/poll/PollConsumer.java
+++ src/main/java/com/nincraft/ninbot/components/poll/PollConsumer.java
@@ -1,13 +1,14 @@
 package com.nincraft.ninbot.components.poll;
 
-import com.nincraft.ninbot.util.MessageUtils;
 import lombok.extern.log4j.Log4j2;
 import lombok.val;
 import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageReaction;
 
-import java.util.ArrayList;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
+import java.util.Timer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
@@ -15,7 +16,6 @@ import java.util.function.Consumer;
 @Log4j2
 class PollConsumer implements Consumer<Message> {
 
-    private static final long MILLISECONDS_CONVERT = 60000;
     private Poll poll;
 
     PollConsumer(Poll poll) {
@@ -26,65 +26,16 @@ class PollConsumer implements Consumer<Message> {
     public void accept(Message message) {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.submit(() -> {
-            try {
-                char digitalOneEmoji = '\u0031';
-                List<String> choices = poll.getChoices();
-                for (int i = 0; i < choices.size(); i++) {
-                    message.addReaction(Character.toString(digitalOneEmoji) + "\u20E3").queue();
-                    digitalOneEmoji++;
-                }
-                Thread.sleep(poll.getTimeLength() * MILLISECONDS_CONVERT);
-                announcePollResults(poll, message);
-            } catch (InterruptedException e) {
-                log.error(e);
-                Thread.currentThread().interrupt();
+            char digitalOneEmoji = '\u0031';
+            List<String> choices = poll.getChoices();
+            for (int i = 0; i < choices.size(); i++) {
+                message.addReaction(Character.toString(digitalOneEmoji) + "\u20E3").queue();
+                digitalOneEmoji++;
             }
+            val announceTime = Instant.now().plus(poll.getTimeLength(), ChronoUnit.MINUTES);
+            PollAnnounce pollAnnounce = new PollAnnounce(poll, message);
+            Timer timer = new Timer();
+            timer.schedule(pollAnnounce, Date.from(announceTime));
         });
-    }
-
-    private void announcePollResults(Poll poll, Message message) {
-        val newMessage = message.getChannel().getMessageById(message.getId()).complete();
-        int highCount = 0;
-        List<String> winningChoices = new ArrayList<>();
-        List<MessageReaction> reactions = newMessage.getReactions();
-        for (int i = 0; i < reactions.size() && i < poll.getChoices().size(); i++) {
-            MessageReaction reaction = reactions.get(i);
-            if (reaction.getCount() == highCount) {
-                winningChoices.add(poll.getChoices().get(i));
-            }
-            if (reaction.getCount() > highCount) {
-                highCount = reaction.getCount();
-                winningChoices.clear();
-                winningChoices.add(poll.getChoices().get(i));
-            }
-        }
-
-        String pollClosedMessage = " The poll is now closed.";
-        if (highCount <= 1) {
-            poll.setResult("No one voted in this poll." + pollClosedMessage);
-        } else if (winningChoices.size() == 1) {
-            poll.setResult("\"" + winningChoices.get(0) + "\" had the most votes with " + (highCount - 1) + " vote(s)."
-                    + pollClosedMessage);
-        } else if (winningChoices.size() > 1) {
-            poll.setResult(
-                    "It's a tie! " + listWinners(winningChoices) + " won with " + (highCount - 1) + " vote(s) each."
-                            + pollClosedMessage);
-        }
-        message.editMessage(poll.buildClosed()).queue();
-        MessageUtils.sendMessage(message.getChannel(), poll.getResult());
-    }
-
-    private String listWinners(List<String> winningChoices) {
-        StringBuilder winners = new StringBuilder();
-        for (int i = 0; i < winningChoices.size(); i++) {
-            if (i == winningChoices.size() - 1) {
-                winners.append("and ");
-            }
-            winners.append("\"").append(winningChoices.get(i)).append("\"");
-            if (i != winningChoices.size() - 1) {
-                winners.append(", ");
-            }
-        }
-        return winners.toString();
     }
 }
