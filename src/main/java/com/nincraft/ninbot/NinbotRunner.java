5e751379f7650ec8bd9f62b8dd15d3bea6746156
Nincodedo <nincodedo@gmail.com>
2018-07-06 21:56:03 -0500

    Refactor

--- src/main/java/com/nincraft/ninbot/NinbotRunner.java
+++ src/main/java/com/nincraft/ninbot/NinbotRunner.java
@@ -2,9 +2,7 @@ package com.nincraft.ninbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.nincraft.ninbot"})
 @SpringBootApplication
 public class NinbotRunner {
     public static void main(String[] args) {
