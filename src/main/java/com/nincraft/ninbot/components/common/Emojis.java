4f5a9f9b113d020c26e78116f441d08e1226e3e1
Nincodedo <nincodedo@gmail.com>
2019-03-02 23:13:42 -0600

    Fix Junits

--- src/main/java/com/nincraft/ninbot/components/common/Emojis.java
+++ src/main/java/com/nincraft/ninbot/components/common/Emojis.java
@@ -1,5 +1,8 @@
 package com.nincraft.ninbot.components.common;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class Emojis {
     public static final String CHECK_MARK = "\u2705";
     public static final String QUESTION_MARK = "\u2754";
