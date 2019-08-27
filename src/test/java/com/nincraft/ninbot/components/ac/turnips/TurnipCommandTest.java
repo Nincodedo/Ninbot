9eaed2b89e51e340ee22af3cf61f857091eb35c8
Nincodedo <nincodedo@gmail.com>
2019-08-26 23:49:14 -0500

    Add TestContainers for JUnits
    Will refactor into integration tests at some point (never)

--- src/test/java/com/nincraft/ninbot/components/ac/turnips/TurnipCommandTest.java
+++ src/test/java/com/nincraft/ninbot/components/ac/turnips/TurnipCommandTest.java
@@ -1,16 +1,32 @@
 package com.nincraft.ninbot.components.ac.turnips;
 
-import com.nincraft.ninbot.NinbotTest;
+import com.nincraft.ninbot.NinbotRunner;
 import com.nincraft.ninbot.TestUtils;
 import com.nincraft.ninbot.components.ac.VillagerManager;
+import com.nincraft.ninbot.components.ac.VillagerRepository;
 import com.nincraft.ninbot.components.common.Emojis;
+import lombok.extern.log4j.Log4j2;
 import lombok.val;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.MySQLContainer;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -18,12 +34,30 @@ import java.time.LocalDate;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-public class TurnipCommandTest extends NinbotTest {
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+@ContextConfiguration(classes = NinbotRunner.class, initializers = {TurnipCommandTest.Initializer.class})
+@TestPropertySource("classpath:application.properties")
+@Log4j2
+public class TurnipCommandTest {
+
+    @ClassRule
+    public static MySQLContainer mySQLContainer = new MySQLContainer();
+
+    static {
+        mySQLContainer.start();
+    }
 
     @Mock
+    public MessageReceivedEvent messageEvent;
+    @Mock
+    public Message message;
+    @MockBean
+    ShardManager shardManager;
+    @Autowired
+    VillagerRepository villagerRepository;
+    @Autowired
     VillagerManager villagerManager;
-
-    @InjectMocks
+    @Autowired
     TurnipCommand turnipCommand;
 
     @Test
@@ -46,6 +80,9 @@ public class TurnipCommandTest extends NinbotTest {
         when(guild.getId()).thenReturn("1");
         val commandResults = turnipCommand.executeCommand(messageEvent);
         assertThat(TestUtils.returnEmoji(commandResults)).contains(Emojis.CHECK_MARK);
+        val villagers = villagerRepository.findAll();
+        villagers.forEach(villager -> log.info("VILLAGER: " + villager));
+        assertThat(villagers).isNotEmpty();
     }
 
     @Test
@@ -57,6 +94,17 @@ public class TurnipCommandTest extends NinbotTest {
             assertThat(TestUtils.returnEmoji(commandResults)).contains(Emojis.CHECK_MARK);
         } else {
             assertThat(TestUtils.returnEmoji(commandResults)).contains(Emojis.CROSS_X);
+        }
+    }
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + mySQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + mySQLContainer.getUsername(),
+                    "spring.datasource.password=" + mySQLContainer.getPassword())
+                    .applyTo(configurableApplicationContext.getEnvironment());
         }
     }
 }
