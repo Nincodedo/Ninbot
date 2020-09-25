eaa40041408a7be13c56ac3d7da67531e8fe41b2
Nincodedo <nincodedo@gmail.com>
2020-09-25 02:55:05 -0500

    Rename Constants

---
+++ src/main/java/dev/nincodedo/ninbot/components/common/Constants.java
@@ -0,0 +1,10 @@
+package dev.nincodedo.ninbot.components.common;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+    public static final String NINBOT_SUPPORTERS_SERVER_ID = "608765747728220161";
+    public static final String NINBOT_DOCUMENTATION_URL = "https://ninbot.nincodedo.dev/";
+    public static final String NINBOT_GITHUB_URL = "https://github.com/Nincodedo/Ninbot";
+}
