6e739e20c2ca4d12eece9771657a047fb7f0d792
Nincodedo <nincodedo@gmail.com>
2020-10-07 13:24:08 -0500

    Add a loading reaction for Cooldown commands

--- src/main/java/dev/nincodedo/ninbot/components/common/Constants.java
+++ src/main/java/dev/nincodedo/ninbot/components/common/Constants.java
@@ -5,6 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constants {
     public static final String NINBOT_SUPPORTERS_SERVER_ID = "608765747728220161";
+    public static final String NINBOT_SERVER_ID = "497444318768922633";
     public static final String NINBOT_DOCUMENTATION_URL = "https://ninbot.nincodedo.dev/";
     public static final String NINBOT_GITHUB_URL = "https://github.com/Nincodedo/Ninbot";
 }
