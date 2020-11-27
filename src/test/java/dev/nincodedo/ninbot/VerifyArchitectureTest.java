9064d37f0270fc6ef7e8785f37e6a2947a076c41
Nincodedo <nincodedo@gmail.com>
2020-11-26 18:43:40 -0600

    Add architecture test

---
+++ src/test/java/dev/nincodedo/ninbot/VerifyArchitectureTest.java
@@ -0,0 +1,20 @@
+package dev.nincodedo.ninbot;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import dev.nincodedo.ninbot.components.command.AbstractCommand;
+import org.junit.jupiter.api.Test;
+
+
+class VerifyArchitectureTest {
+    @Test
+    void testCommandClasses() {
+        JavaClasses ninbotClasses = new ClassFileImporter().importPackages("dev.nincodedo.ninbot");
+        ArchRule rule = ArchRuleDefinition.classes().that().haveNameMatching(".*Command")
+                .and().haveNameNotMatching("Abstract.*")
+                .should().beAssignableTo(AbstractCommand.class);
+        rule.check(ninbotClasses);
+    }
+}
