726012c6353d7404550fb33b1f6c8f7e2ce8d242
Nincodedo <nincodedo@gmail.com>
2021-01-26 23:36:53 -0600

    Refactor stream announcements
    Stream announcements should now be a bit more consistent

---
+++ src/main/java/dev/nincodedo/ninbot/components/stream/StreamMessageBuilder.java
@@ -0,0 +1,35 @@
+package dev.nincodedo.ninbot.components.stream;
+
+import dev.nincodedo.ninbot.components.common.LocaleService;
+import dev.nincodedo.ninbot.components.common.message.MessageBuilderHelper;
+import lombok.extern.log4j.Log4j2;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Message;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+@Log4j2
+public class StreamMessageBuilder {
+
+    Message buildStreamAnnounceMessage(String avatarUrl, String username,
+            String streamingUrl, String gameName, String streamTitle, String serverId, Locale locale) {
+        log.trace("Building stream announce message for {} server {}", username, serverId);
+        ResourceBundle resourceBundle = LocaleService.getResourceBundleOrDefault(locale);
+        EmbedBuilder embedBuilder;
+        if (!streamingUrl.contains("https://")) {
+            embedBuilder = new EmbedBuilder()
+                    .setAuthor(String.format(resourceBundle.getString("listener.stream.announce.voicechannel"),
+                            username, streamingUrl), null, avatarUrl)
+                    .setTitle(streamTitle);
+        } else {
+            embedBuilder = new EmbedBuilder()
+                    .setAuthor(String.format(resourceBundle.getString("listener.stream.announce"), username, gameName,
+                            streamingUrl), streamingUrl, avatarUrl)
+                    .setTitle(streamTitle);
+        }
+        embedBuilder.setColor(MessageBuilderHelper.getColor(avatarUrl));
+        return new MessageBuilder(embedBuilder).build();
+    }
+}
--- src/main/java/dev/nincodedo/ninbot/components/stream/StreamingMember.java
+++ src/main/java/dev/nincodedo/ninbot/components/stream/StreamingMember.java
@@ -3,11 +3,12 @@ package dev.nincodedo.ninbot.components.stream;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 @Data
 @Entity
@@ -20,7 +21,13 @@ class StreamingMember {
     private String userId;
     private String guildId;
     private String twitchUsername;
-    private boolean currentlyStreaming = false;
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            mappedBy = "streamingMember",
+            fetch = FetchType.EAGER
+    )
+    private List<StreamInstance> streamInstances = new ArrayList<>();
 
     public StreamingMember() {
 
@@ -30,5 +37,16 @@ class StreamingMember {
         this.userId = userId;
         this.guildId = guildId;
         this.started = LocalDateTime.now();
+    }
+
+    public void startNewStream() {
+        streamInstances.add(new StreamInstance());
+    }
+
+    public Optional<StreamInstance> currentStream() {
+        return streamInstances.stream()
+                .sorted(Comparator.comparing(StreamInstance::getStartTimestamp))
+                .filter(streamInstance -> streamInstance.getEndTimestamp() != null)
+                .findFirst();
     }
 }
