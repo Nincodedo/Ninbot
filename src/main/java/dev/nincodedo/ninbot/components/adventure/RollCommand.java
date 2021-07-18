559944e99d1560b08fb8cf6aa41acf688f2c025f
Nincodedo <Nincodedo@users.noreply.github.com>
2021-07-18 16:50:25 -0500

    Add Roll slash command (#528)

--- src/main/java/dev/nincodedo/ninbot/components/adventure/RollCommand.java
+++ src/main/java/dev/nincodedo/ninbot/components/adventure/RollCommand.java
@@ -6,15 +6,22 @@ import com.bernardomg.tabletop.dice.interpreter.DiceRoller;
 import com.bernardomg.tabletop.dice.parser.DefaultDiceParser;
 import com.bernardomg.tabletop.dice.parser.DiceParser;
 import dev.nincodedo.ninbot.components.command.AbstractCommand;
+import dev.nincodedo.ninbot.components.command.SlashCommand;
 import dev.nincodedo.ninbot.components.common.message.MessageAction;
 import lombok.val;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import org.springframework.stereotype.Component;
 
+import java.util.*;
+
 @Component
-public class RollCommand extends AbstractCommand {
+public class RollCommand extends AbstractCommand implements SlashCommand {
 
     private final DiceParser parser;
 
@@ -34,22 +41,49 @@ public class RollCommand extends AbstractCommand {
         val content = event.getMessage().getContentStripped();
         val commandArgs = content.split("\\s+");
         if (commandArgs.length == 2) {
-            messageAction.addChannelAction(rollDice("1d20", event));
+            messageAction.addChannelAction(rollDice("1d20", event.getMember().getEffectiveName()));
         } else if (commandArgs.length == 3 && commandArgs[2].contains("d")) {
-            messageAction.addChannelAction(rollDice(commandArgs[2], event));
+            messageAction.addChannelAction(rollDice(commandArgs[2], event.getMember().getEffectiveName()));
         } else {
             messageAction.addUnknownReaction();
         }
         return messageAction;
     }
 
-    private Message rollDice(String diceArgs, MessageReceivedEvent event) {
+    private Message rollDice(String diceArgs, String memberEffectiveName) {
         val parsed = parser.parse(diceArgs, roller);
         val diceCommand = diceArgs.split("d");
         MessageBuilder messageBuilder = new MessageBuilder();
-        messageBuilder.appendFormat(resourceBundle.getString("command.roll.result"), event.getMember()
-                        .getEffectiveName(), diceCommand[0], diceCommand[1], String.valueOf(parsed.getTotalRoll()),
-                parsed.toString());
+        messageBuilder.appendFormat(resourceBundle.getString("command.roll.result"), memberEffectiveName,
+                diceCommand[0], diceCommand[1], String
+                .valueOf(parsed.getTotalRoll()), parsed.toString());
         return messageBuilder.build();
+    }
+
+    @Override
+    public String getDescription() {
+        return ResourceBundle.getBundle("lang", Locale.ENGLISH).getString("command.roll.description.text");
+    }
+
+    @Override
+    public List<OptionData> getCommandOptions() {
+        return Arrays.asList(new OptionData(OptionType.STRING, "notation", "Simple dice notation"));
+    }
+
+    @Override
+    public List<SubcommandData> getSubcommandDatas() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void execute(SlashCommandEvent slashCommandEvent) {
+        if (slashCommandEvent.getOption("notation") != null) {
+            String notation = slashCommandEvent.getOption("notation").getAsString();
+            slashCommandEvent.reply(rollDice(notation, slashCommandEvent.getMember().getEffectiveName())).queue();
+        } else if (slashCommandEvent.getOption("notation") == null) {
+            slashCommandEvent.reply(rollDice("1d20", slashCommandEvent.getMember().getEffectiveName())).queue();
+        } else {
+            slashCommandEvent.reply("wot").queue();
+        }
     }
 }
