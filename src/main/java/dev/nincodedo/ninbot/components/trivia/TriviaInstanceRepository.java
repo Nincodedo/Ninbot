28f3cc0e3bb9dbb6535b4d2638e5f4584ec7b920
Nincodedo <nincodedo@gmail.com>
2021-09-12 16:43:55 -0500

    Remove unused

--- src/main/java/dev/nincodedo/ninbot/components/trivia/TriviaInstanceRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.nincodedo.ninbot.components.trivia;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TriviaInstanceRepository extends CrudRepository<TriviaInstance, Long> {
-    boolean existsByChannelId(String channelId);
-
-    void deleteByChannelId(String channelId);
-}
--- src/main/java/dev/nincodedo/ninbot/components/trivia/game/opentdb/TriviaCategory.java
+++
@@ -1,9 +0,0 @@
-package dev.nincodedo.ninbot.components.trivia.game.opentdb;
-
-import lombok.Data;
-
-@Data
-public class TriviaCategory {
-    private Integer id;
-    private String name;
-}
