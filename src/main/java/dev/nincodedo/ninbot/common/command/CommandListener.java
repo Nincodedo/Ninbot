4b45687515f9b83a9a2cbce225278354d403cb52
Nincodedo <nincodedo@gmail.com>
2021-09-15 22:50:26 -0500

    Remove old mention commands
    o7

--- src/main/java/dev/nincodedo/ninbot/common/command/CommandListener.java
+++ src/main/java/dev/nincodedo/ninbot/common/command/CommandListener.java
@@ -1,12 +1,10 @@
 package dev.nincodedo.ninbot.common.command;
 
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
-import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Nonnull;
 import java.util.List;
 
 @Component
@@ -14,10 +12,8 @@ public class CommandListener extends ListenerAdapter {
 
     private CommandParser commandParser;
 
-    public CommandListener(CommandParser commandParser, List<AbstractCommand> commands,
-            List<SlashCommand> slashCommands) {
+    public CommandListener(CommandParser commandParser, List<SlashCommand> slashCommands) {
         this.commandParser = commandParser;
-        addCommands(commands);
         addSlashCommands(slashCommands);
     }
 
@@ -25,25 +21,8 @@ public class CommandListener extends ListenerAdapter {
         commandParser.addSlashCommands(slashCommands);
     }
 
-    private void addCommands(List<AbstractCommand> commands) {
-        commandParser.registerAliases(commands);
-        commandParser.addCommands(commands);
-    }
-
-    @Override
-    public void onPrivateMessageReceived(@Nonnull PrivateMessageReceivedEvent event) {
-        if (isNinbotMention(event)) {
-            commandParser.parseEvent(event);
-        }
-    }
-
     @Override
     public void onSlashCommand(@NotNull SlashCommandEvent slashCommandEvent) {
         commandParser.parseEvent(slashCommandEvent);
-    }
-
-    private boolean isNinbotMention(PrivateMessageReceivedEvent event) {
-        return !event.getAuthor().isBot()
-                && event.getMessage().getContentStripped().toLowerCase().startsWith("@ninbot");
     }
 }
--- src/main/java/dev/nincodedo/ninbot/common/command/CommandParser.java
+++ src/main/java/dev/nincodedo/ninbot/common/command/CommandParser.java
@@ -1,13 +1,8 @@
 package dev.nincodedo.ninbot.common.command;
 
-import dev.nincodedo.ninbot.components.config.component.ComponentService;
-import dev.nincodedo.ninbot.components.config.component.ComponentType;
 import io.micrometer.core.instrument.util.NamedThreadFactory;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
-import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -16,69 +11,15 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static dev.nincodedo.ninbot.common.Emojis.QUESTION_MARK;
-
 @Slf4j
 @Component
 public class CommandParser {
 
-    private ComponentService componentService;
-    @Getter
-    private Map<String, AbstractCommand> commandHashMap = new HashMap<>();
     private Map<String, SlashCommand> slashCommandMap = new HashMap<>();
-    private Map<String, String> commandAliasMap = new HashMap<>();
     private ExecutorService executorService;
 
-    CommandParser(ComponentService componentService) {
+    CommandParser() {
         this.executorService = Executors.newCachedThreadPool(new NamedThreadFactory("command-parser"));
-        this.componentService = componentService;
-    }
-
-    void parseEvent(PrivateMessageReceivedEvent event) {
-        String message = event.getMessage().getContentStripped();
-        if (StringUtils.isNotBlank(message)) {
-            String commandName = getCommand(message);
-            AbstractCommand command = commandHashMap.get(commandName);
-            if (command != null) {
-                try {
-                    executorService.execute(() -> command.execute(event));
-                } catch (Exception e) {
-                    log.error("Error executing command " + command.getName(), e);
-                }
-            } else {
-                event.getMessage().addReaction(QUESTION_MARK).queue();
-            }
-        }
-    }
-
-    private String getCommand(String message) {
-        String[] splitMessage = message.split("\\s+");
-        if (splitMessage.length > 1) {
-            var commandName = translateAlias(splitMessage[1]);
-            return commandName != null ? commandName.toLowerCase() : StringUtils.EMPTY;
-        }
-        return null;
-    }
-
-    private String translateAlias(String alias) {
-        var commandName = commandAliasMap.get(alias);
-        return commandName != null ? commandName : alias;
-    }
-
-    void addCommands(List<AbstractCommand> commands) {
-        commands.forEach(this::addCommand);
-    }
-
-    void addCommand(AbstractCommand command) {
-        componentService.registerComponent(command.getName(), ComponentType.COMMAND);
-        commandHashMap.put(command.getName(), command);
-    }
-
-    void registerAliases(List<AbstractCommand> commands) {
-        for (var command : commands) {
-            command.getAliases().forEach(alias -> commandAliasMap.put(alias, command.getName()));
-            commandAliasMap.put(command.getName(), command.getName());
-        }
     }
 
     public void parseEvent(SlashCommandEvent slashCommandEvent) {
