cce58869161b719ed0ddecd2f3707be0b317e64a
Nincodedo <nincodedo@gmail.com>
2021-09-18 23:35:33 -0500

    Log potential slash command registration errors

--- src/main/java/dev/nincodedo/ninbot/common/command/SlashCommandRegistration.java
+++ src/main/java/dev/nincodedo/ninbot/common/command/SlashCommandRegistration.java
@@ -1,6 +1,7 @@
 package dev.nincodedo.ninbot.common.command;
 
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
@@ -22,17 +23,23 @@ public class SlashCommandRegistration extends ListenerAdapter {
     public void onReady(ReadyEvent readyEvent) {
         var shardManager = readyEvent.getJDA().getShardManager();
         if (shardManager != null) {
-            shardManager.getGuilds()
-                    .forEach(guild -> {
-                        if (guild != null) {
-                            guild.updateCommands().complete();
-                            List<CommandData> commandDataList = slashCommands.stream()
-                                    .map(this::convertToCommandData)
-                                    .toList();
-                            guild.updateCommands().addCommands(commandDataList).queue();
-                        }
-                    }
-            );
+            shardManager.getGuilds().forEach(this::registerCommands);
+        }
+    }
+
+    private void registerCommands(Guild guild) {
+        if (guild != null) {
+            try {
+                guild.updateCommands().complete();
+                List<CommandData> commandDataList = slashCommands.stream()
+                        .map(this::convertToCommandData)
+                        .toList();
+                guild.updateCommands().addCommands(commandDataList).queue();
+            } catch (Exception e) {
+                log.error("Failed to register commands on server {}", guild.getId(), e);
+            }
+        } else {
+            log.trace("Null guild found?");
         }
     }
 
