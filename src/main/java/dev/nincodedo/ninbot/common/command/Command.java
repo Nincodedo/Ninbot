8bcf9101e8d72e119b9239d4f46454b0ffbe743b
Nincodedo <nincodedo@gmail.com>
2022-04-07 16:57:34 -0500

    Some Javadoc

--- src/main/java/dev/nincodedo/ninbot/common/command/Command.java
+++ src/main/java/dev/nincodedo/ninbot/common/command/Command.java
@@ -7,5 +7,10 @@ import java.util.Locale;
 public interface Command extends ReleaseStage {
     Locale defaultLocale = Locale.ENGLISH;
 
+    /**
+     * Returns the name of the Command.
+     *
+     * @return String name
+     */
     String getName();
 }
