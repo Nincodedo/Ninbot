eac7c1642c7ec2bf8e91c876a0bc84cf215589b8
Nincodedo <nincodedo@gmail.com>
2022-04-07 20:40:07 -0500

    Fix tests

--- src/main/java/dev/nincodedo/ninbot/components/datetime/TimeCommandName.java
+++ src/main/java/dev/nincodedo/ninbot/components/datetime/TimeCommandName.java
@@ -2,7 +2,7 @@ package dev.nincodedo.ninbot.components.datetime;
 
 import dev.nincodedo.ninbot.common.command.CommandNameEnum;
 
-public enum TimeCommandName implements CommandNameEnum {
+enum TimeCommandName implements CommandNameEnum {
     TIME;
 
     enum Option implements CommandNameEnum {
