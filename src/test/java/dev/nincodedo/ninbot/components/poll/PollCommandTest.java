1d53c3bf047cea2308d130910f007b14a34ee085
Nincodedo <nincodedo@gmail.com>
2022-04-09 12:58:12 -0500

    Fix tests

--- src/test/java/dev/nincodedo/ninbot/components/poll/PollCommandTest.java
+++ src/test/java/dev/nincodedo/ninbot/components/poll/PollCommandTest.java
@@ -14,6 +14,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
@@ -22,11 +24,13 @@ import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @ContextConfiguration(classes = {NinbotRunner.class})
 @TestPropertySource(locations = {"classpath:application.properties", "classpath:ninbot.properties"})
+@MockitoSettings(strictness = Strictness.LENIENT)
 class PollCommandTest {
 
     @Mock
@@ -41,19 +45,14 @@ class PollCommandTest {
         TextChannel textChannel = Mockito.mock(TextChannel.class);
         Guild guild = Mockito.mock(Guild.class);
         ReplyCallbackAction replyAction = Mockito.mock(ReplyCallbackAction.class);
-        OptionMapping choice1 = Mockito.mock(OptionMapping.class);
-        OptionMapping choice2 = Mockito.mock(OptionMapping.class);
-        OptionMapping questionOption = Mockito.mock(OptionMapping.class);
         when(slashCommandEvent.getChannel()).thenReturn(textChannel);
         when(slashCommandEvent.getGuild()).thenReturn(guild);
         when(slashCommandEvent.getMember()).thenReturn(member);
-        when(slashCommandEvent.getOption("choice1")).thenReturn(choice1);
-        when(slashCommandEvent.getOption("choice2")).thenReturn(choice2);
-        when(slashCommandEvent.getOption("question")).thenReturn(questionOption);
+        when(slashCommandEvent.getOption(eq("choice1"), any())).thenReturn("1st");
+        when(slashCommandEvent.getOption(eq("choice2"), any())).thenReturn("2nd");
+        when(slashCommandEvent.getOption(eq("question"), any())).thenReturn("why?");
+        when(slashCommandEvent.getOption(eq("polllength"), eq(5L), any())).thenReturn(5L);
         when(slashCommandEvent.reply(any(Message.class))).thenReturn(replyAction);
-        when(choice1.getAsString()).thenReturn("1st");
-        when(choice2.getAsString()).thenReturn("2nd");
-        when(questionOption.getAsString()).thenReturn("why?");
         when(guild.getId()).thenReturn("1");
         when(textChannel.getId()).thenReturn("1");
         when(member.getEffectiveAvatarUrl()).thenReturn("http://google.com/a-url");
@@ -70,21 +69,17 @@ class PollCommandTest {
         Member member = Mockito.mock(Member.class);
         TextChannel textChannel = Mockito.mock(TextChannel.class);
         Guild guild = Mockito.mock(Guild.class);
-        OptionMapping choice1 = Mockito.mock(OptionMapping.class);
-        OptionMapping choice2 = Mockito.mock(OptionMapping.class);
         OptionMapping choice3 = Mockito.mock(OptionMapping.class);
-        OptionMapping questionOption = Mockito.mock(OptionMapping.class);
         when(slashCommandEvent.getChannel()).thenReturn(textChannel);
         when(slashCommandEvent.getGuild()).thenReturn(guild);
-        when(slashCommandEvent.getOption("choice1")).thenReturn(choice1);
-        when(slashCommandEvent.getOption("choice2")).thenReturn(choice2);
+        when(slashCommandEvent.getOption(eq("choice1"), any())).thenReturn("1st");
+        when(slashCommandEvent.getOption(eq("choice2"), any())).thenReturn("2nd");
         when(slashCommandEvent.getOptions()).thenReturn(List.of(choice3));
         when(choice3.getName()).thenReturn("choice3");
         when(choice3.getAsString()).thenReturn("3rd");
-        when(slashCommandEvent.getOption("question")).thenReturn(questionOption);
-        when(questionOption.getAsString()).thenReturn("test");
+        when(slashCommandEvent.getOption(eq("question"), any())).thenReturn("test");
         when(slashCommandEvent.getOption("userchoices")).thenReturn(null);
-        when(slashCommandEvent.getOption("polllength")).thenReturn(null);
+        when(slashCommandEvent.getOption(eq("polllength"), eq(5L), any())).thenReturn(5L);
         when(guild.getId()).thenReturn("1");
         when(textChannel.getId()).thenReturn("1");
         when(member.getEffectiveAvatarUrl()).thenReturn("http://avatarturl.com/avatar.png");
