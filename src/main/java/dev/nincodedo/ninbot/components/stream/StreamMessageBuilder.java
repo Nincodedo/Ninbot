59ad3fc3bcafde2d6cecda77a02bad6d9f101cac
Nincodedo <nincodedo@gmail.com>
2022-04-11 22:28:37 -0500

    Refactor

--- src/main/java/dev/nincodedo/ninbot/components/stream/StreamMessageBuilder.java
+++ src/main/java/dev/nincodedo/ninbot/components/stream/StreamMessageBuilder.java
@@ -1,35 +1,39 @@
 package dev.nincodedo.ninbot.components.stream;
 
 import dev.nincodedo.ninbot.common.LocaleService;
+import dev.nincodedo.ninbot.common.logging.UtilLogging;
 import dev.nincodedo.ninbot.common.message.MessageUtils;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 
-import java.util.Locale;
 import java.util.ResourceBundle;
 
 @Slf4j
 public class StreamMessageBuilder {
 
-    Message buildStreamAnnounceMessage(String avatarUrl, String username,
-            String streamingUrl, String gameName, String streamTitle, String serverId, Locale locale) {
-        log.trace("Building stream announce message for {} server {}", username, serverId);
-        ResourceBundle resourceBundle = LocaleService.getResourceBundleOrDefault(locale);
+    Message buildStreamAnnounceMessage(Member member,
+            String streamingUrl, String gameName, String streamTitle, Guild guild) {
+        log.trace("Building stream announce message for {} server {}", UtilLogging.logMemberInfo(member),
+                UtilLogging.logGuildName(guild));
+        ResourceBundle resourceBundle = LocaleService.getResourceBundleOrDefault(guild.getLocale());
         EmbedBuilder embedBuilder;
         if (!streamingUrl.contains("https://")) {
             embedBuilder = new EmbedBuilder()
                     .setAuthor(String.format(resourceBundle.getString("listener.stream.announce.voicechannel"),
-                            username, streamingUrl), null, avatarUrl)
+                            member.getEffectiveName(), streamingUrl), null, member.getAvatarUrl())
                     .setTitle(streamTitle);
         } else {
             embedBuilder = new EmbedBuilder()
-                    .setAuthor(String.format(resourceBundle.getString("listener.stream.announce"), username, gameName,
-                            streamingUrl), streamingUrl, avatarUrl)
+                    .setAuthor(String.format(resourceBundle.getString("listener.stream.announce"),
+                            member.getEffectiveName(), gameName,
+                            streamingUrl), streamingUrl, member.getAvatarUrl())
                     .setTitle(streamTitle);
         }
-        embedBuilder.setColor(MessageUtils.getColor(avatarUrl));
+        embedBuilder.setColor(MessageUtils.getColor(member.getAvatarUrl()));
         return new MessageBuilder(embedBuilder).build();
     }
 }
