716f0d5c0ac1fde5aadb02b13654d3e2c1fe5433
Nincodedo <nincodedo@gmail.com>
2022-09-09 15:52:58 -0500

    Add response for failed Wordles

--- src/main/java/dev/nincodedo/ninbot/components/reaction/ReactionResponse.java
+++ src/main/java/dev/nincodedo/ninbot/components/reaction/ReactionResponse.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Data
 public class ReactionResponse {
@@ -33,7 +34,7 @@ public class ReactionResponse {
         return switch (reactionMatchType) {
             case EXACT -> getTarget().equalsIgnoreCase(event.getMessage().getContentStripped());
             case CONTAINS -> event.getMessage().getContentStripped().toLowerCase().contains(getTarget().toLowerCase());
-            case REGEX -> event.getMessage().getContentStripped().matches(getTarget());
+            case REGEX -> Pattern.matches(getTarget(), event.getMessage().getContentStripped().replace("\n",""));
         };
     }
 }
