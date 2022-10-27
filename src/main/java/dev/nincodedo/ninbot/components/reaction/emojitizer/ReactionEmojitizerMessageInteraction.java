2dfe1cbe06bf62394b0bfd5bbf68317ed5154b5d
Nincodedo <nincodedo@gmail.com>
2022-10-27 13:11:20 -0500

    Add case for full message

--- src/main/java/dev/nincodedo/ninbot/components/reaction/emojitizer/ReactionEmojitizerMessageInteraction.java
+++ src/main/java/dev/nincodedo/ninbot/components/reaction/emojitizer/ReactionEmojitizerMessageInteraction.java
@@ -3,6 +3,7 @@ package dev.nincodedo.ninbot.components.reaction.emojitizer;
 import dev.nincodedo.ninbot.common.command.message.MessageContextCommand;
 import dev.nincodedo.ninbot.common.message.MessageContextInteractionEventMessageExecutor;
 import dev.nincodedo.ninbot.common.message.MessageExecutor;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
@@ -21,15 +22,21 @@ public class ReactionEmojitizerMessageInteraction implements MessageContextComma
     @Override
     public MessageExecutor execute(@NotNull MessageContextInteractionEvent event) {
         var messageExecutor = new MessageContextInteractionEventMessageExecutor(event);
-        TextInput input = TextInput.create("emojitizer-text", "What do you want to Emojitize?",
-                        TextInputStyle.SHORT)
-                .setMinLength(1)
-                .setMaxLength(20)
-                .build();
-        Modal modal = Modal.create("emojitizer-modal-" + event.getTarget().getId(), "Emojitizer")
-                .addActionRow(input)
-                .build();
-        event.replyModal(modal).queue();
+        var currentEmojiCount = event.getTarget().getReactions().size();
+        if (currentEmojiCount >= Message.MAX_REACTIONS) {
+            messageExecutor.addEphemeralMessage("Sorry, that message is already full of emojis!");
+        } else {
+            var maxCount = Message.MAX_REACTIONS - currentEmojiCount;
+            TextInput input = TextInput.create("emojitizer-text", "What do you want to Emojitize?",
+                            TextInputStyle.SHORT)
+                    .setMinLength(1)
+                    .setMaxLength(maxCount)
+                    .build();
+            Modal modal = Modal.create("emojitizer-modal-" + event.getTarget().getId(), "Emojitizer - ")
+                    .addActionRow(input)
+                    .build();
+            event.replyModal(modal).queue();
+        }
         return messageExecutor;
     }
 }
