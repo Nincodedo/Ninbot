23f7ee0470fcb5cfaa9eb63499c6e81e50b76755
Nincodedo <nincodedo@gmail.com>
2022-11-04 21:56:04 -0500

    Rebase fixes

--- src/main/java/dev/nincodedo/ninbot/components/stream/StreamingMemberRepository.java
+++ src/main/java/dev/nincodedo/ninbot/components/stream/StreamingMemberRepository.java
@@ -1,18 +1,13 @@
 package dev.nincodedo.ninbot.components.stream;
 
-import org.jetbrains.annotations.NotNull;
-import org.springframework.data.repository.CrudRepository;
 import dev.nincodedo.ninbot.common.persistence.BaseRepository;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface StreamingMemberRepository extends BaseRepository<StreamingMember> {
     Optional<StreamingMember> findByUserIdAndGuildId(String userId, String guildId);
-
-    @NotNull
-    @Override
-    List<StreamingMember> findAll();
 
     @NotNull
     List<StreamingMember> findAllByTwitchUsernameIsNotNull();
