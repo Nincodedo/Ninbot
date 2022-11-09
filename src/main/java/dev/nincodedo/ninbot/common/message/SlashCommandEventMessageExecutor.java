c88a790312fea45a710cef16f24bd5fe5ef47695
Nincodedo <nincodedo@gmail.com>
2022-11-08 18:17:39 -0600

    Add modal to EphemeralMessageExecutor

--- src/main/java/dev/nincodedo/ninbot/common/message/SlashCommandEventMessageExecutor.java
+++ src/main/java/dev/nincodedo/ninbot/common/message/SlashCommandEventMessageExecutor.java
@@ -4,6 +4,8 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.modals.Modal;
+import net.dv8tion.jda.api.requests.restaction.interactions.ModalCallbackAction;
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import org.jetbrains.annotations.NotNull;
@@ -27,6 +29,11 @@ public class SlashCommandEventMessageExecutor extends EphemeralMessageExecutor {
     @Override
     public Message getMessage() {
         return overrideMessage;
+    }
+
+    @Override
+    protected ModalCallbackAction replyModal(Modal modal) {
+        return slashCommandEvent.replyModal(modal);
     }
 
     public ReplyCallbackAction replyMessage(MessageCreateData message) {
