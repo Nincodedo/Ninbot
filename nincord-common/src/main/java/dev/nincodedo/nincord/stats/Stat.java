39360b98ce82db69905e270de85cea64837108ff
Nincodedo <nincodedo@gmail.com>
2022-12-06 13:48:58 -0600

    Stat refactor

--- nincord-common/src/main/java/dev/nincodedo/nincord/stats/Stat.java
+++ nincord-common/src/main/java/dev/nincodedo/nincord/stats/Stat.java
@@ -3,10 +3,14 @@ package dev.nincodedo.nincord.stats;
 
 import dev.nincodedo.nincord.persistence.BaseEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 @Entity
 @Data
 public class Stat extends BaseEntity {
@@ -15,4 +19,10 @@ public class Stat extends BaseEntity {
     private String name;
     private Integer count = 0;
     private String serverId;
+
+    public Stat(String name, String category, String serverId) {
+        this.name = name;
+        this.category = category;
+        this.serverId = serverId;
+    }
 }
