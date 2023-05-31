c7f7dd2928b7c218f06a477e704456377980ed2d
Nincodedo <nincodedo@gmail.com>
2023-05-31 14:34:46 -0500

    Fix docs

--- ninbot-app/src/main/java/dev/nincodedo/ninbot/components/stream/banner/GameBannerBuilder.java
+++ ninbot-app/src/main/java/dev/nincodedo/ninbot/components/stream/banner/GameBannerBuilder.java
@@ -87,7 +87,8 @@ public abstract class GameBannerBuilder {
     }
 
     /**
-     * Generates 3 GameBanners, returning only 1, or null if one could not be created. Others are stored in cache.
+     * Generates zero to three GameBanners, returning only 1, or null if one could not be created. Others are stored
+     * in cache.
      *
      * @param gameTitle String title of a game
      * @return a GameBanner or null if one could not be created
