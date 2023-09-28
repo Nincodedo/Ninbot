964cc89e3312aff60429a3e0fd851a9d08ee62b8
Nincodedo <Nincodedo@users.noreply.github.com>
2023-09-28 18:06:54 -0500

    Fix deleted polls not closing (#917)

--- ninbot-app/src/main/java/dev/nincodedo/ninbot/components/poll/PollScheduler.java
+++ ninbot-app/src/main/java/dev/nincodedo/ninbot/components/poll/PollScheduler.java
@@ -2,15 +2,19 @@ package dev.nincodedo.ninbot.components.poll;
 
 import dev.nincodedo.nincord.LocaleService;
 import dev.nincodedo.nincord.Schedulable;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.channel.concrete.NewsChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.exceptions.ErrorHandler;
+import net.dv8tion.jda.api.requests.ErrorResponse;
 import net.dv8tion.jda.api.sharding.ShardManager;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutorService;
 
+@Slf4j
 @Component
 public class PollScheduler extends Schedulable<Poll, PollService> {
 
@@ -44,7 +48,13 @@ public class PollScheduler extends Schedulable<Poll, PollService> {
             default -> throw new IllegalStateException("Unexpected value: " + guildChannel.getType());
         };
         channel.retrieveMessageById(poll.getMessageId())
-                .queue(message -> pollAnnouncementSetup.setupAnnounce(poll, shardManager, message));
+                .queue(message -> pollAnnouncementSetup.setupAnnounce(poll, shardManager, message),
+                        new ErrorHandler().handle(ErrorResponse.UNKNOWN_MESSAGE, error -> {
+                            log.warn("Could not find message {} for poll {}. Closing the poll.", poll.getMessageId(),
+                                    poll.getId());
+                            poll.setPollOpen(false);
+                            pollService.save(poll);
+                        }));
     }
 
     @Override
