b7c3e600404a89c313293c6c794ece846776fb86
Nincodedo <nincodedo@gmail.com>
2025-05-31 02:45:41 -0500

    Add reshake button to Magic 8 Ball

--- ninbot-app/src/main/java/dev/nincodedo/ninbot/components/eightball/Magic8BallMessageBuilder.java
+++ ninbot-app/src/main/java/dev/nincodedo/ninbot/components/eightball/Magic8BallMessageBuilder.java
@@ -2,7 +2,11 @@ package dev.nincodedo.ninbot.components.eightball;
 
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.utils.messages.MessageEditBuilder;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -23,7 +27,7 @@ public class Magic8BallMessageBuilder {
     private Random random;
     private List<String> eightBallAnswers;
     private String question;
-    private String memberName;
+    private String userName;
 
     public Magic8BallMessageBuilder(@Value("classpath:magic8BallAnswers.txt") Resource answerFile) {
         this.random = new SecureRandom();
@@ -32,15 +36,19 @@ public class Magic8BallMessageBuilder {
 
     MessageEmbed build() {
         EmbedBuilder embedBuilder = new EmbedBuilder();
-        if (StringUtils.isEmpty(StringUtils.trimToEmpty(question))) {
-            embedBuilder.setTitle("%s shakes the Magic 8 Ball".formatted(memberName));
-        } else {
-            embedBuilder.setTitle("%s shakes the Magic 8 Ball and asks \"%s\"".formatted(memberName, question));
-        }
+        setEmbedBuilderTitle(embedBuilder);
         embedBuilder.setThumbnail("https://i.imgur.com/80imnZ0.png");
         embedBuilder.appendDescription("The Magic 8 Ball says...\n\n");
         embedBuilder.appendDescription("_" + getMagic8BallAnswer() + "_");
         return embedBuilder.build();
+    }
+
+    private void setEmbedBuilderTitle(EmbedBuilder embedBuilder) {
+        if (StringUtils.isEmpty(StringUtils.trimToEmpty(question))) {
+            embedBuilder.setTitle("%s shakes the Magic 8 Ball".formatted(userName));
+        } else {
+            embedBuilder.setTitle("%s shakes the Magic 8 Ball and asks \"%s\"".formatted(userName, question));
+        }
     }
 
     private String getMagic8BallAnswer() {
@@ -65,8 +73,42 @@ public class Magic8BallMessageBuilder {
         return this;
     }
 
-    public Magic8BallMessageBuilder memberName(String effectiveName) {
-        this.memberName = effectiveName;
+    public Magic8BallMessageBuilder userName(String effectiveName) {
+        this.userName = effectiveName;
         return this;
+    }
+
+    public MessageEditBuilder reshake(Message message, int maxShakes) {
+        var editMessage = MessageEditBuilder.fromMessage(message);
+        var embed = editMessage.getEmbeds().getFirst();
+        var editEmbed = EmbedBuilder.fromData(embed.toData());
+        editEmbed.setDescription(null);
+        var footerText = embed.getFooter() != null ? embed.getFooter().getText() : null;
+        var shakeCount = getNextShakeCount(maxShakes, footerText);
+        editEmbed.setFooter("%s shakes".formatted(shakeCount));
+        if (shakeCount >= maxShakes) {
+            editMessage.setComponents(ActionRow.of(Button.secondary("empty", "No Shakes Left").asDisabled()));
+            editEmbed.appendDescription("After a final shake, the Magic 8 Ball says...\n\n");
+        } else {
+            editEmbed.appendDescription("After another shake, the Magic 8 Ball says...\n\n");
+        }
+        editEmbed.appendDescription("_" + getMagic8BallAnswer() + "_");
+        editMessage.setEmbeds(editEmbed.build());
+        return editMessage;
+    }
+
+    protected int getNextShakeCount(int maxShakes, String footerText) {
+        int currentShakeCount;
+        if (footerText == null) {
+            currentShakeCount = 1;
+        } else {
+            try {
+                currentShakeCount = Integer.parseInt(footerText.split(" ")[0]);
+            } catch (NumberFormatException e) {
+                log.warn("Couldn't get a shake count from footer {}, setting shake count to max.", footerText);
+                currentShakeCount = maxShakes;
+            }
+        }
+        return currentShakeCount + 1;
     }
 }
---
+++ ninbot-app/src/main/java/dev/nincodedo/ninbot/components/eightball/Magic8BallReshakeButtonInteraction.java
@@ -0,0 +1,52 @@
+package dev.nincodedo.ninbot.components.eightball;
+
+import dev.nincodedo.nincord.command.component.ButtonInteraction;
+import dev.nincodedo.nincord.command.component.ComponentData;
+import dev.nincodedo.nincord.config.db.ConfigConstants;
+import dev.nincodedo.nincord.config.db.ConfigService;
+import dev.nincodedo.nincord.message.ButtonInteractionCommandMessageExecutor;
+import dev.nincodedo.nincord.message.MessageExecutor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class Magic8BallReshakeButtonInteraction implements ButtonInteraction {
+
+    private final Magic8BallMessageBuilder magic8BallMessageBuilder;
+    private final ConfigService configService;
+
+    @Override
+    public MessageExecutor execute(@NotNull ButtonInteractionEvent event,
+            @NotNull ButtonInteractionCommandMessageExecutor messageExecutor, @NotNull ComponentData componentData) {
+        event.deferEdit().queue();
+        var expectedUserId = componentData.data();
+        if (!expectedUserId.equals(event.getUser().getId())) {
+            return messageExecutor;
+        }
+        var message = event.getMessage();
+        var serverId = event.getGuild() != null ? event.getGuild().getId() : "0";
+        var maxShakesConfigOptional = configService.getGlobalConfigByName(ConfigConstants.MAGIC_EIGHT_BALL_MAX_SHAKES
+                , serverId);
+        int maxShakes;
+        maxShakes = maxShakesConfigOptional.map(config -> Integer.parseInt(config.getValue())).orElse(8);
+        var editMessage = magic8BallMessageBuilder.reshake(message, maxShakes);
+        event.getHook().editOriginal(editMessage.build()).queue();
+        return messageExecutor;
+    }
+
+    @Override
+    public Logger log() {
+        return log;
+    }
+
+    @Override
+    public String getName() {
+        return Magic8BallCommandName.EIGHTBALL.get();
+    }
+}
