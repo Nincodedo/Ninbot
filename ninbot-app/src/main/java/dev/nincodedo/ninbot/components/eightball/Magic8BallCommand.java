60f1764690e80de17aa3e59b44e987b87a379089
Nincodedo <Nincodedo@users.noreply.github.com>
2025-05-31 18:59:52 -0500

    Add reshake button to Magic 8 Ball (#1105)
    * Add reshake button to Magic 8 Ball
    
    * Update build-pr.yml
    
    * Small fixes

--- ninbot-app/src/main/java/dev/nincodedo/ninbot/components/eightball/Magic8BallCommand.java
+++ ninbot-app/src/main/java/dev/nincodedo/ninbot/components/eightball/Magic8BallCommand.java
@@ -3,35 +3,37 @@ package dev.nincodedo.ninbot.components.eightball;
 import dev.nincodedo.nincord.command.slash.SlashCommand;
 import dev.nincodedo.nincord.message.MessageExecutor;
 import dev.nincodedo.nincord.message.SlashCommandEventMessageExecutor;
+import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class Magic8BallCommand implements SlashCommand {
 
-    private Magic8BallMessageBuilder magic8BallMessageBuilder;
-
-    public Magic8BallCommand(Magic8BallMessageBuilder magic8BallMessageBuilder) {
-        this.magic8BallMessageBuilder = magic8BallMessageBuilder;
-    }
+    private final Magic8BallMessageBuilder magic8BallMessageBuilder;
 
     @Override
-    public MessageExecutor execute(
-            @NotNull SlashCommandInteractionEvent event,
+    public MessageExecutor execute(@NotNull SlashCommandInteractionEvent event,
             @NotNull SlashCommandEventMessageExecutor messageExecutor) {
-        var question = event.getOption(Magic8BallCommandName.Option.QUESTION.get(), "",
-                OptionMapping::getAsString);
-        var messageEmbed = magic8BallMessageBuilder
-                .question(question)
-                .memberName(event.getMember().getEffectiveName())
+        var question = event.getOption(Magic8BallCommandName.Option.QUESTION.get(), "", OptionMapping::getAsString);
+        var messageEmbed = magic8BallMessageBuilder.question(question)
+                .userName(event.getUser().getEffectiveName())
                 .build();
-        messageExecutor.addMessageEmbed(messageEmbed);
+        var response = new MessageCreateBuilder().addEmbeds(messageEmbed)
+                .addComponents(ActionRow.of(Button.primary("%s-reshake-%s".formatted(getName(), event.getUser()
+                        .getId()), "Shake Again")))
+                .build();
+        messageExecutor.addMessageResponse(response);
         return messageExecutor;
     }
 
@@ -42,7 +44,7 @@ public class Magic8BallCommand implements SlashCommand {
 
     @Override
     public List<OptionData> getCommandOptions() {
-        return List.of(new OptionData(OptionType.STRING, Magic8BallCommandName.Option.QUESTION.get(), "Your "
-                + "question to the 8 ball."));
+        return List.of(new OptionData(OptionType.STRING, Magic8BallCommandName.Option.QUESTION.get(), "Your question "
+                + "to the 8 ball."));
     }
 }
