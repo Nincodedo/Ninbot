b993d21c0419e6973ea2aef50b388deafe4d8771
Nincodedo <Nincodedo@users.noreply.github.com>
2025-12-03 18:38:07 -0600

    Add numbers as valid Haiku responses (#1147)
    * Add numbers to Haiku responses
    
    * Fix tests

--- ninbot-app/src/test/java/dev/nincodedo/ninbot/components/haiku/HaikuMessageParserTest.java
+++ ninbot-app/src/test/java/dev/nincodedo/ninbot/components/haiku/HaikuMessageParserTest.java
@@ -3,10 +3,8 @@ package dev.nincodedo.ninbot.components.haiku;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,24 +13,14 @@ class HaikuMessageParserTest {
 
     HaikuMessageParser haikuMessageParser = new HaikuMessageParser();
 
-    static List<String> nonhaikuables() {
-        return List.of("too short", "the the the the the the the the the the the the the the the the 9",
-                "Because Amazon continues to be a curse on my work life.");
-    }
-
-    static List<String> haikuables() {
-        return List.of("the the the the the the the the the the the the the the the the the",
-                "Because Amazon continues to be a curse on my whole work life.");
-    }
-
     @ParameterizedTest
-    @MethodSource("nonhaikuables")
+    @ArgumentsSource(NonhaikuableArgumentsProvider.class)
     void messagesUnhaikuable(String unhaikuable) {
         assertThat(haikuMessageParser.isHaikuable(unhaikuable)).isNotPresent();
     }
 
     @ParameterizedTest
-    @MethodSource("haikuables")
+    @ArgumentsSource(HaikuableArgumentsProvider.class)
     void messageHaikuable(String haikuable) {
         assertThat(haikuMessageParser.isHaikuable(haikuable)).isPresent();
     }
